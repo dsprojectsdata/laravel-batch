82960a270bc20f2402c1a5476fbc394a8bfcdb6a
debuggersolutions <debuggersolutions@gmail.com>
2023-12-22 20:29:11 +0530

    theme setup

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\admin\AdminHomeController;
 use App\Http\Controllers\web\BlogController;
 use App\Http\Controllers\web\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::view('/', 'welcome');
 
-Route::get('/', [HomeController::class, 'index'])->name('homePage');
-Route::get('/blogs', [BlogController::class, 'index'])->name('blogIndex');
+
+
+Route::prefix('/')->group(function () {
+      Route::get('/', [HomeController::class, 'index'])->name('homePage');
+      Route::get('/blogs', [BlogController::class, 'index'])->name('blogIndex');
+});
+
+
+Route::prefix('admin')->group(function () {
+      Route::get('/', [AdminHomeController::class, 'index'])->name('admin.dashboard');
+});
